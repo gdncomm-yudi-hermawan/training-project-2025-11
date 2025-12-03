0d72d7fb2e5326ae4f17f048c15295ea5c9a9fff
Yudi Hermawan <yudi.hermawan@gdn-commerce.com>
2025-12-03 18:29:27 +0700

    feat: Introduce API Gateway module and adjust parent Spring Boot version.

--- api-gateway/src/main/java/com/marketplace/gateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/marketplace/gateway/ApiGatewayApplication.java
@@ -2,8 +2,10 @@ package com.marketplace.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = { "com.marketplace.gateway", "com.marketplace.common" })
 public class ApiGatewayApplication {
 
 	public static void main(String[] args) {
